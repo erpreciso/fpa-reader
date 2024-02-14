77417d2d5747dca525b447494152c877c47284bc
erpreciso <trepreciso@gmail.com>
2024-02-14 17:50:28 +0100

    Select which fields to export.

--- fpa-schema.el
+++ fpa-schema.el
@@ -11,30 +11,30 @@
        (("1.1.5.1" do-not-export "Telefono" nil)
         ("1.1.5.2" do-not-export "Email" nil)))
       ("1.1.6" do-not-export "PECDestinatario" nil)))
-    ("1.2" do-not-export "CedentePrestatore"
-     (("1.2.1" do-not-export "DatiAnagrafici"
-       (("1.2.1.1" do-not-export "IdFiscaleIVA"
-         (("1.2.1.1.1" do-not-export "IdPaese" nil)
-          ("1.2.1.1.2" do-not-export "IdCodice" nil)))
-        ("1.2.1.2" do-not-export "CodiceFiscale" nil)
-        ("1.2.1.3" do-not-export "Anagrafica"
-         (("1.2.1.3.1" do-not-export "Denominazione" nil)
-          ("1.2.1.3.2" do-not-export "Nome" nil)
-          ("1.2.1.3.3" do-not-export "Cognome" nil)
+    ("1.2" export "CedentePrestatore"
+     (("1.2.1" export "DatiAnagrafici"
+       (("1.2.1.1" export "IdFiscaleIVA"
+         (("1.2.1.1.1" export "IdPaese" nil)
+          ("1.2.1.1.2" export "IdCodice" nil)))
+        ("1.2.1.2" export "CodiceFiscale" nil)
+        ("1.2.1.3" export "Anagrafica"
+         (("1.2.1.3.1" export "Denominazione" nil)
+          ("1.2.1.3.2" export "Nome" nil)
+          ("1.2.1.3.3" export "Cognome" nil)
           ("1.2.1.3.4" do-not-export "Titolo" nil)
           ("1.2.1.3.5" do-not-export "CodEORI" nil)))
         ("1.2.1.4" do-not-export "AlboProfessionale" nil)
         ("1.2.1.5" do-not-export "ProvinciaAlbo" nil)
         ("1.2.1.6" do-not-export "NumeroIscrizioneAlbo" nil)
         ("1.2.1.7" do-not-export "DataIscrizioneAlbo" nil)
-        ("1.2.1.8" do-not-export "RegimeFiscale" nil)))
-      ("1.2.2" do-not-export "Sede"
+        ("1.2.1.8" export "RegimeFiscale" nil)))
+      ("1.2.2" export "Sede"
        (("1.2.2.1" do-not-export "Indirizzo" nil)
         ("1.2.2.2" do-not-export "NumeroCivico" nil)
         ("1.2.2.3" do-not-export "CAP" nil)
-        ("1.2.2.4" do-not-export "Comune" nil)
-        ("1.2.2.5" do-not-export "Provincia" nil)
-        ("1.2.2.6" do-not-export "Nazione" nil)))
+        ("1.2.2.4" export "Comune" nil)
+        ("1.2.2.5" export "Provincia" nil)
+        ("1.2.2.6" export "Nazione" nil)))
       ("1.2.3" do-not-export "StabileOrganizzazione"
        (("1.2.3.1" do-not-export "Indirizzo" nil)
         ("1.2.3.2" do-not-export "NumeroCivico" nil)
@@ -65,25 +65,25 @@
           ("1.3.1.3.3" do-not-export "Cognome" nil)
           ("1.3.1.3.4" do-not-export "Titolo" nil)
           ("1.3.1.3.5" do-not-export "CodEORI" nil)))))))
-    ("1.4" do-not-export "CessionarioCommittente"
-     (("1.4.1" do-not-export "DatiAnagrafici"
-       (("1.4.1.1" do-not-export "IdFiscaleIVA"
+    ("1.4" export "CessionarioCommittente"
+     (("1.4.1" export "DatiAnagrafici"
+       (("1.4.1.1" export "IdFiscaleIVA"
          (("1.4.1.1.1" do-not-export "IdPaese" nil)
-          ("1.4.1.1.2" do-not-export "IdCodice" nil)))
-        ("1.4.1.2" do-not-export "CodiceFiscale" nil)
-        ("1.4.1.3" do-not-export "Anagrafica"
-         (("1.4.1.3.1" do-not-export "Denominazione" nil)
-          ("1.4.1.3.2" do-not-export "Nome" nil)
-          ("1.4.1.3.3" do-not-export "Cognome" nil)
+          ("1.4.1.1.2" export "IdCodice" nil)))
+        ("1.4.1.2" export "CodiceFiscale" nil)
+        ("1.4.1.3" export "Anagrafica"
+         (("1.4.1.3.1" export "Denominazione" nil)
+          ("1.4.1.3.2" export "Nome" nil)
+          ("1.4.1.3.3" export "Cognome" nil)
           ("1.4.1.3.4" do-not-export "Titolo" nil)
           ("1.4.1.3.5" do-not-export "CodEORI" nil)))))
-      ("1.4.2" do-not-export "Sede"
+      ("1.4.2" export "Sede"
        (("1.4.2.1" do-not-export "Indirizzo" nil)
         ("1.4.2.2" do-not-export "NumeroCivico" nil)
         ("1.4.2.3" do-not-export "CAP" nil)
-        ("1.4.2.4" do-not-export "Comune" nil)
-        ("1.4.2.5" do-not-export "Provincia" nil)
-        ("1.4.2.6" do-not-export "Nazione" nil)))
+        ("1.4.2.4" export "Comune" nil)
+        ("1.4.2.5" export "Provincia" nil)
+        ("1.4.2.6" export "Nazione" nil)))
       ("1.4.3" do-not-export "StabileOrganizzazione"
        (("1.4.3.1" do-not-export "Indirizzo" nil)
         ("1.4.3.2" do-not-export "NumeroCivico" nil)
@@ -111,13 +111,13 @@
           ("1.5.1.3.4" do-not-export "Titolo" nil)
           ("1.5.1.3.5" do-not-export "CodEORI" nil)))))))
     ("1.6" do-not-export "SoggettoEmittente" nil)))
-  ("2" do-not-export "FatturaElettronicaBody"
-   (("2.1" do-not-export "DatiGenerali"
-     (("2.1.1" do-not-export "DatiGeneraliDocumento"
-       (("2.1.1.1" do-not-export "TipoDocumento" nil)
-        ("2.1.1.2" do-not-export "Divisa" nil)
-        ("2.1.1.3" do-not-export "Data" nil)
-        ("2.1.1.4" do-not-export "Numero" nil)
+  ("2" export "FatturaElettronicaBody"
+   (("2.1" export "DatiGenerali"
+     (("2.1.1" export "DatiGeneraliDocumento"
+       (("2.1.1.1" export "TipoDocumento" nil)
+        ("2.1.1.2" export "Divisa" nil)
+        ("2.1.1.3" export "Data" nil)
+        ("2.1.1.4" export "Numero" nil)
         ("2.1.1.5" do-not-export "DatiRitenuta"
          (("2.1.1.5.1" do-not-export "TipoRitenuta" nil)
           ("2.1.1.5.2" do-not-export "ImportoRitenuta" nil)
@@ -139,9 +139,9 @@
          (("2.1.1.8.1" do-not-export "Tipo" nil)
           ("2.1.1.8.2" do-not-export "Percentuale" nil)
           ("2.1.1.8.3" do-not-export "Importo" nil)))
-        ("2.1.1.9" do-not-export "ImportoTotaleDocumento" nil)
-        ("2.1.1.10" do-not-export "Arrotondamento" nil)
-        ("2.1.1.11" do-not-export "Causale" nil)
+        ("2.1.1.9" export "ImportoTotaleDocumento" nil)
+        ("2.1.1.10" export "Arrotondamento" nil)
+        ("2.1.1.11" export "Causale" nil)
         ("2.1.1.12" do-not-export "Art73" nil)))
       ("2.1.2" do-not-export "DatiOrdineAcquisto"
        (("2.1.2.1" do-not-export "RiferimentoNumeroLinea" nil)
@@ -195,25 +195,25 @@
       ("2.1.10" do-not-export "FatturaPrincipale"
        (("2.1.10.1" do-not-export "NumeroFatturaPrincipale" nil)
         ("2.1.10.2" do-not-export "DataFatturaPrincipale" nil)))))
-    ("2.2" do-not-export "DatiBeniServizi"
-     (("2.2.1" do-not-export "DettaglioLinee"
-       (("2.2.1.1" do-not-export "NumeroLinea" nil)
+    ("2.2" export "DatiBeniServizi"
+     (("2.2.1" export "DettaglioLinee"
+       (("2.2.1.1" export "NumeroLinea" nil)
         ("2.2.1.2" do-not-export "TipoCessionePrestazione" nil)
-        ("2.2.1.3" do-not-export "CodiceArticolo"
-         (("2.2.1.3.1" do-not-export "CodiceTipo" nil)
-          ("2.2.1.3.2" do-not-export "CodiceValore" nil)))
-        ("2.2.1.4" do-not-export "Descrizione" nil)
-        ("2.2.1.5" do-not-export "Quantita" nil)
-        ("2.2.1.6" do-not-export "UnitaMisura" nil)
+        ("2.2.1.3" export "CodiceArticolo"
+         (("2.2.1.3.1" export "CodiceTipo" nil)
+          ("2.2.1.3.2" export "CodiceValore" nil)))
+        ("2.2.1.4" export "Descrizione" nil)
+        ("2.2.1.5" export "Quantita" nil)
+        ("2.2.1.6" export "UnitaMisura" nil)
         ("2.2.1.7" do-not-export "DataInizioPeriodo" nil)
         ("2.2.1.8" do-not-export "DataFinePeriodo" nil)
-        ("2.2.1.9" do-not-export "PrezzoUnitario" nil)
+        ("2.2.1.9" export "PrezzoUnitario" nil)
         ("2.2.1.10" do-not-export "ScontoMaggiorazione"
          (("2.2.1.10.1" do-not-export "Tipo" nil)
           ("2.2.1.10.2" do-not-export "Percentuale" nil)
           ("2.2.1.10.3" do-not-export "Importo" nil)))
-        ("2.2.1.11" do-not-export "PrezzoTotale" nil)
-        ("2.2.1.12" do-not-export "AliquotaIVA" nil)
+        ("2.2.1.11" export "PrezzoTotale" nil)
+        ("2.2.1.12" export "AliquotaIVA" nil)
         ("2.2.1.13" do-not-export "Ritenuta" nil)
         ("2.2.1.14" do-not-export "Natura" nil)
         ("2.2.1.15" do-not-export "RiferimentoAmministrazione" nil)
@@ -234,15 +234,15 @@
     ("2.3" do-not-export "DatiVeicoli"
      (("2.3.1" do-not-export "Data" nil)
       ("2.3.2" do-not-export "TotalePercorso" nil)))
-    ("2.4" do-not-export "DatiPagamento"
-     (("2.4.1" do-not-export "CondizioniPagamento" nil)
-      ("2.4.2" do-not-export "DettaglioPagamento"
-       (("2.4.2.1" do-not-export "Beneficiario" nil)
-        ("2.4.2.2" do-not-export "ModalitaPagamento" nil)
-        ("2.4.2.3" do-not-export "DataRiferimentoTerminiPagamento" nil)
-        ("2.4.2.4" do-not-export "GiorniTerminiPagamento" nil)
-        ("2.4.2.5" do-not-export "DataScadenzaPagamento" nil)
-        ("2.4.2.6" do-not-export "ImportoPagamento" nil)
+    ("2.4" export "DatiPagamento"
+     (("2.4.1" export "CondizioniPagamento" nil)
+      ("2.4.2" export "DettaglioPagamento"
+       (("2.4.2.1" export "Beneficiario" nil)
+        ("2.4.2.2" export "ModalitaPagamento" nil)
+        ("2.4.2.3" export "DataRiferimentoTerminiPagamento" nil)
+        ("2.4.2.4" export "GiorniTerminiPagamento" nil)
+        ("2.4.2.5" export "DataScadenzaPagamento" nil)
+        ("2.4.2.6" export "ImportoPagamento" nil)
         ("2.4.2.7" do-not-export "CodUfficioPostale" nil)
         ("2.4.2.8" do-not-export "CognomeQuietanzante" nil)
         ("2.4.2.9" do-not-export "NomeQuietanzante" nil)
